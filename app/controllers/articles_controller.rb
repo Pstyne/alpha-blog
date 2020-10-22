793a90ca1bdfb3615a59819c6603950da9e2220a
Andrew Birt <abirt1986@gmail.com>
2020-10-21 23:40:54 -0400

    add sessions, helpers, configure views to use helper methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,8 +24,7 @@ class ArticlesController < ApplicationController
   # POST /articles
   # POST /articles.json
   def create
-    @article = Article.new(article_params)
-    @article.user = User.first
+    @article = helpers.current_user.articles.build(article_params)
     respond_to do |format|
       if @article.save
         format.html { redirect_to @article, notice: 'Article was successfully created.' }
