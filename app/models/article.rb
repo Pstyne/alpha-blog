a1e210ea9c1938be221da7b4a0e4cfecb0287080
Andrew Birt <abirt1986@gmail.com>
2020-10-23 13:16:07 -0400

    create many to many association for articles and categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 10, maximum: 150 }
   validates :content, presence: true, length: { minimum: 10 }
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
 end
